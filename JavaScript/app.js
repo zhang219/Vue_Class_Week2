67c4f7a144cd7bdac973a177defbc332ccb70892
Wendy <cyw880219@gmail.com>
2022-01-23 11:13:26 +0800

    新增app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -12,14 +12,17 @@ createApp({
     methods:{
         login() {
             const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
-            axios.post(api, this.user).then((response) => {
-                const { token, expired } = response.data;
-                //寫入 cookie token
-                //expires 設置有效時間
-                document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/`;
-                window.location = 'products.html';
-            }).catch((error) => {
-                alert(error.data.message);
+            axios.post(api, this.user)
+                .then((response) => {
+                    const { token, expired } = response.data;
+                    //寫入 cookie token
+                    //expires 設置有效時間
+                    document.cookie = `hexToken=${ token }; expires=${ new Date(expired) };`;
+                    //somecookie自訂義名稱 = 值，expires是時間格式 用 newDate來做轉型
+                    window.location = 'products.html';
+                })
+                .catch((error) => {
+                    alert(error.response.data.message);
             });
         },
     },
